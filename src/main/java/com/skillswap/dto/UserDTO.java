f797008280b2057580bc66ada2404e7f54474b57
Harshita Hegde <harshitavhegde1234@gmail.com>
2026-07-04 15:51:12 +0530

    Add UserDTO

---
+++ src/main/java/com/skillswap/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.skillswap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String bio;
+    private Double averageRating;
+    private Integer totalReviews;
+}

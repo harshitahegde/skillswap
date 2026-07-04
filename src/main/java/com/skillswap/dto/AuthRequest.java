79cb7882fff6fd75683520b6cb32242d9f4840bc
Harshita Hegde <harshitavhegde1234@gmail.com>
2026-07-04 15:39:06 +0530

    Add AuthRequest DTO

---
+++ src/main/java/com/skillswap/dto/AuthRequest.java
@@ -0,0 +1,25 @@
+package com.skillswap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
